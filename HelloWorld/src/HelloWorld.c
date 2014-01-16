0cc4d6b6f71371807cd1655c1770f24a23016dd5
unknown <Poulomi_Roy01@BLRKEC250572D.ad.infosys.com>
2014-01-16 10:29:40 +0530

    initial

--- HelloWorld/src/HelloWorld.c
+++ HelloWorld/src/HelloWorld.c
@@ -8,10 +8,17 @@
  ============================================================================
  */
 
+
 #include <stdio.h>
+
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+int main(void) 
+{
+	
+puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	
+return EXIT_SUCCESS;
+
 }
